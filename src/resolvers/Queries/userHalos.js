cf329419308910defff2d2d849d2b5da62bdbc9c
Michael Jones <jonesmichael3.1415@gmail.com>
2019-01-31 22:34:45 -0500

    39

--- src/resolvers/Queries/userHalos.js
+++ src/resolvers/Queries/userHalos.js
@@ -7,7 +7,10 @@ module.exports = async (parent, { username }, ctx) => {
     .user({ username })
     .halos({ orderBy: 'createdAt_DESC' });
   // Grab each of the Halo's info
-  const data = map(halos, halo => ctx.utils.halos.grabHalo(halo.key, halo.tier));
+  const data = map(halos, halo => ({
+    ...ctx.utils.halos.grabHalo(halo.key, halo.tier),
+    date: halo.createdAt,
+  }));
   // Return just the highest tier of each Halo
   return uniqBy(data, 'key');
 };
