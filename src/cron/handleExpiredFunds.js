3121bcd9c8e72ba4bbf6623026c592eb0dbb72e6
Michael Jones <jonesmichael3.1415@gmail.com>
2019-08-12 17:27:57 -0400

    75

--- src/cron/handleExpiredFunds.js
+++ src/cron/handleExpiredFunds.js
@@ -42,52 +42,55 @@ module.exports = (binding, client, sendEmail, grabEvents, stripe) => {
     // There is at least one charge that needs to be auto donated
     if (charges.length) {
       // Grab the current event info
-      const [{ id: eventID, charity: { connectedAccountID, name: charityName } }] = await grabEvents.current({ first: 1 }, '{ id charity { connectedAccountID name} }');
-      log('Charges:');
-      // Donate each of the charges
-      each(charges, async ({ amountNet, chargeID, donations, id: sourceID, user: { id: userID, email, nameFirst, nameLast, transactions } }) => {
-        // Log the charge's ID
-        log(`- ${sourceID}`);
-        // User's current balance
-        const [{ balance }] = transactions;
-        // Amount of the source funds left
-        const amount = donations.length ? donations[0].chargeBalance : amountNet;
-        // Balance after donating
-        const newBalance = balance - amount;
-        // Create the transaction that links to the donation
-        const { createdAt: date, id: transactionID } = await client.createTransaction({
-          balance: newBalance,
-          user: { connect: { id: userID } },
-        });
-        // Transfer the funds in Stripe
-        stripe.transfers.create({
-          amount,
-          currency: 'usd',
-          destination: connectedAccountID,
-          source_transaction: chargeID,
-        }).then(({ id: transferID }) => (
-          // Add the donation to Phenominal and link it to the transaction
-          client.createTransfer({
+      const activeEvent = await grabEvents.current({ first: 1 }, '{ id charity { connectedAccountID name} }');
+      if (activeEvent) {
+        const [{ id: eventID, charity: { connectedAccountID, name: charityName } }] = activeEvent;
+        log('Charges:');
+        // Donate each of the charges
+        each(charges, async ({ amountNet, chargeID, donations, id: sourceID, user: { id: userID, email, nameFirst, nameLast, transactions } }) => {
+          // Log the charge's ID
+          log(`- ${sourceID}`);
+          // User's current balance
+          const [{ balance }] = transactions;
+          // Amount of the source funds left
+          const amount = donations.length ? donations[0].chargeBalance : amountNet;
+          // Balance after donating
+          const newBalance = balance - amount;
+          // Create the transaction that links to the donation
+          const { createdAt: date, id: transactionID } = await client.createTransaction({
+            balance: newBalance,
+            user: { connect: { id: userID } },
+          });
+          // Transfer the funds in Stripe
+          stripe.transfers.create({
             amount,
-            chargeBalance: 0,
-            firstOfBatch: true,
-            transferID,
-            event: { connect: { id: eventID } },
-            source: { connect: { id: sourceID } },
-            transaction: { connect: { id: transactionID } },
-          })
-        ));
-        // Information that is used in the confirmation
-        const transactionData = {
-          amount,
-          balance: newBalance,
-          charityName,
-          date,
-          transactionID,
-        };
-        // Send an email that the funds where automatically donated
-        sendEmail.expiredFundsDonated(transactionData, email, nameFirst, nameLast);
-      });
+            currency: 'usd',
+            destination: connectedAccountID,
+            source_transaction: chargeID,
+          }).then(({ id: transferID }) => (
+            // Add the donation to Phenominal and link it to the transaction
+            client.createTransfer({
+              amount,
+              chargeBalance: 0,
+              firstOfBatch: true,
+              transferID,
+              event: { connect: { id: eventID } },
+              source: { connect: { id: sourceID } },
+              transaction: { connect: { id: transactionID } },
+            })
+          ));
+          // Information that is used in the confirmation
+          const transactionData = {
+            amount,
+            balance: newBalance,
+            charityName,
+            date,
+            transactionID,
+          };
+          // Send an email that the funds where automatically donated
+          sendEmail.expiredFundsDonated(transactionData, email, nameFirst, nameLast);
+        });
+      } else log('No Active Event');
     } else log('No Charges');
   };
   // Run script at 12:05AM everyday
